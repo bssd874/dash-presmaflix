661d13618a7fc7267e92034029eb4426fec5d3e6
Zilfs <rheinlandcast@gmail.com>
2023-07-07 15:10:59 +0700

    add: form content and video

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,186 +1,45 @@
-import { useCallback } from "react";
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  Divider,
-  TextField,
-  Unstable_Grid2 as Grid,
-} from "@mui/material";
+import Head from "next/head";
+import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid } from "@mui/material";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import { AccountProfileDetails } from "src/sections/content-section/content-details";
 
-// const states = [
-//   {
-//     value: "alabama",
-//     label: "Alabama",
-//   },
-//   {
-//     value: "new-york",
-//     label: "New York",
-//   },
-//   {
-//     value: "san-francisco",
-//     label: "San Francisco",
-//   },
-//   {
-//     value: "los-angeles",
-//     label: "Los Angeles",
-//   },
-// ];
-
-const type = [
-  {
-    value: "movie",
-    label: "Movie",
-  },
-  {
-    value: "tv-global",
-    label: "TV",
-  },
-  {
-    value: "music-video",
-    label: "Music Video",
-  },
-];
-
-export const AccountProfileDetails = () => {
-  const handleChange = useCallback((event) => {
-    setValues((prevState) => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  }, []);
-
-  const handleSubmit = useCallback((event) => {
-    event.preventDefault();
-  }, []);
-
-  return (
-    <form autoComplete="off" noValidate onSubmit={handleSubmit}>
-      <Card>
-        <CardHeader subheader="The information can be edited" title="Content Details" />
-        <CardContent sx={{ pt: 0 }}>
-          <Box sx={{ m: -1.5 }}>
+const Page = () => (
+  <>
+    <Head>
+      <title>Form Input Content | Devias Kit</title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Stack spacing={3}>
+          <div>
+            <Typography variant="h4">Insert New Content</Typography>
+          </div>
+          <div>
             <Grid container spacing={3}>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  // helperText="Please specify the first name"
-                  label="Judul"
-                  name="title"
-                  onChange={handleChange}
-                  required
-                />
-              </Grid>
-              {/* <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Last name"
-                  name="lastName"
-                  onChange={handleChange}
-                  required
-                  value={values.lastName}
-                />
+              {/* <Grid
+                xs={12}
+                md={6}
+                lg={4}
+              >
+                <AccountProfile />
               </Grid> */}
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Pilih Tipe"
-                  name="type"
-                  onChange={handleChange}
-                  required
-                  select
-                  SelectProps={{ native: true }}
-                >
-                  {type.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </TextField>
+              <Grid xs={12} md={12} lg={12}>
+                <AccountProfileDetails />
               </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Direksi"
-                  name="direksi"
-                  onChange={handleChange}
-                  required
-                  placeholder="Joko Anwar, Teddy Soeriaatmadja, Eddie Cahyono"
-                  helperText="Tambahkan koma jika ingin menambahkan direksi lebih dari satu"
-                />
-              </Grid>
-
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Pemeran"
-                  name="cast"
-                  required
-                  onChange={handleChange}
-                  placeholder="Tom Hanks, Reza Rahadian"
-                  helperText="Tambahkan koma jika ingin menambahkan pemeran lebih dari satu"
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Thumbnail"
-                  name="thumnailUrl"
-                  onChange={handleChange}
-                  required
-                  helperText="Masukkan URL Gambar"
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Poster"
-                  name="posterUrl"
-                  onChange={handleChange}
-                  required
-                  helperText="Masukkan URL Gambar"
-                />
-              </Grid>
-              <Grid xs={12} md={12}>
-                <TextField
-                  fullWidth
-                  multiline
-                  label="Deskripsi"
-                  name="description"
-                  onChange={handleChange}
-                  required
-                />
-              </Grid>
-
-              {/* <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Select State"
-                  name="state"
-                  onChange={handleChange}
-                  required
-                  select
-                  SelectProps={{ native: true }}
-                  value={values.state}
-                >
-                  {states.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </TextField>
-              </Grid> */}
             </Grid>
-          </Box>
-        </CardContent>
-        <Divider />
-        <CardActions sx={{ justifyContent: "flex-end" }}>
-          <Button variant="contained">Save</Button>
-        </CardActions>
-      </Card>
-    </form>
-  );
-};
+          </div>
+        </Stack>
+      </Container>
+    </Box>
+  </>
+);
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
---
+++ src/sections/content-section/content-details.js
@@ -0,0 +1,131 @@
+import { useCallback } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
+
+const type = [
+  {
+    value: "movie",
+    label: "Movie",
+  },
+  {
+    value: "tv-global",
+    label: "TV",
+  },
+  {
+    value: "music-video",
+    label: "Music Video",
+  },
+];
+
+export const AccountProfileDetails = () => {
+  const handleChange = useCallback((event) => {
+    setValues((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  }, []);
+
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+  }, []);
+
+  return (
+    <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader subheader="The information can be edited" title="Content Details" />
+        <CardContent sx={{ pt: 0 }}>
+          <Box sx={{ m: -1.5 }}>
+            <Grid container spacing={3}>
+              <Grid xs={12} md={6}>
+                <TextField fullWidth label="Judul" name="title" onChange={handleChange} required />
+              </Grid>
+              <Grid xs={12} md={6}>
+                <TextField
+                  fullWidth
+                  label="Pilih Tipe"
+                  name="type"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                >
+                  {type.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid xs={12} md={6}>
+                <TextField
+                  fullWidth
+                  label="Direksi"
+                  name="direksi"
+                  onChange={handleChange}
+                  required
+                  placeholder="Joko Anwar, Teddy Soeriaatmadja, Eddie Cahyono"
+                  helperText="Tambahkan koma jika ingin menambahkan direksi lebih dari satu"
+                />
+              </Grid>
+
+              <Grid xs={12} md={6}>
+                <TextField
+                  fullWidth
+                  label="Pemeran"
+                  name="cast"
+                  required
+                  onChange={handleChange}
+                  placeholder="Tom Hanks, Reza Rahadian"
+                  helperText="Tambahkan koma jika ingin menambahkan pemeran lebih dari satu"
+                />
+              </Grid>
+              <Grid xs={12} md={6}>
+                <TextField
+                  fullWidth
+                  label="Thumbnail"
+                  name="thumnailUrl"
+                  onChange={handleChange}
+                  required
+                  helperText="Masukkan URL Gambar"
+                />
+              </Grid>
+              <Grid xs={12} md={6}>
+                <TextField
+                  fullWidth
+                  label="Poster"
+                  name="posterUrl"
+                  onChange={handleChange}
+                  required
+                  helperText="Masukkan URL Gambar"
+                />
+              </Grid>
+              <Grid xs={12} md={12}>
+                <TextField
+                  fullWidth
+                  multiline
+                  label="Deskripsi"
+                  name="description"
+                  onChange={handleChange}
+                  required
+                />
+              </Grid>
+            </Grid>
+          </Box>
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ justifyContent: "flex-end" }}>
+          <Button variant="contained">Save</Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+};
